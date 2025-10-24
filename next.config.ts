6f101ba8f34bd7741bf7561ce8b3de93fcd314f8
Fabrício <fab.brito94@gmail.com>
2025-10-24 17:49:40 -0300

    feat: add image configuration with minimum cache TTL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    minimumCacheTTL: 30 * 24 * 60 * 60, // 30 days
+  },
 };
 
 export default nextConfig;
